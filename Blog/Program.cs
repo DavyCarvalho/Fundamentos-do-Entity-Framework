5020a193ec92dda48b2e312ccec17a8f4a15998d
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-08 20:15:00 -0300

    Creating Database query, Blog Console App and Models

---
+++ Blog/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Data Source=NT-03072\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True";
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
