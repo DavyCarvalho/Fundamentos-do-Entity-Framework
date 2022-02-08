0a40412e30ae88cbdea7fd6d59e8d5be84af44a0
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-08 20:31:02 -0300

    Creating Data Base Context for EF and adding connection string for local database

---
+++ Blog/Data/BlogDataContext.cs
@@ -0,0 +1,21 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=NT-03072\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True");
+        }
+    }
+}
