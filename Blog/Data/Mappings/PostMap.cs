dc62969d0c7ce04c17696c2dbd57b43a1de13d9c
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-09 20:38:23 -0300

    Relationship one to one

--- Blog/Data/Mappings/PostMap.cs
+++ Blog/Data/Mappings/PostMap.cs
@@ -31,6 +31,19 @@ namespace Blog.Data.Mappings
             builder
                 .HasIndex(x => x.Slug, "IX_Post_Slug")
                 .IsUnique();
+
+            // Relacionamentos Um para Um (1:1)
+            builder
+                .HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasOne(x => x.Category)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Category")
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
