6219c7965c4480a880373548bcfa619a539e33c2
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-08 21:15:34 -0300

    EF Create Command

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,12 +1,21 @@
-﻿namespace Blog
+﻿using Blog.Data;
+using Blog.Models;
+
+namespace Blog
 {
     class Program
     {
-        private const string CONNECTION_STRING = @"Data Source=NT-03072\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True";
-
         static void Main(string[] args)
         {
+            using (var context = new BlogDataContext())
+            {
+                //Create
+                var tag = new Tag { Name = ".NET", Slug = "dotnet" };
+                context.Tags.Add(tag);
+                context.SaveChanges();
 
+                
+            }
         }
     }
 }
