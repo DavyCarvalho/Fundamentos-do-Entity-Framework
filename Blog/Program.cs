812f36159b99a8653b4b7fad0d21775bb0d0ef21
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-09 21:00:35 -0300

    testing table relationships

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Blog.Data;
 using Blog.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -8,11 +7,41 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using (var context = new BlogDataContext())
+            using var context = new BlogDataContext();
+
+            // // Incluindo um Usuário
+            // context.Users.Add(new User
+            // {
+            //     Bio = "9x Microsoft MVP",
+            //     Email = "balta@mail.com",
+            //     Image = "http://balta.io",
+            //     Name = "André",
+            //     PasswordHash = "1234",
+            //     Slug = "andre-balta"
+            // });
+            // context.SaveChanges();
+
+            var user = context.Users.FirstOrDefault(); // recuperando o User que adicionamos acima no banco
+
+            var post = new Post
             {
-                
-            }
+                Author = user,
+                Body = "Meu Artigo",
+                Category = new Category
+                {
+                    Name = "Backend",
+                    Slug = "backend"
+                },
+                CreateDate = DateTime.Now,
+                //LastUpdateDate = Comentado para testar a função Default do Banco
+                Slug = "meu-artigo",
+                Summary = "Neste artigo vamos...",
+                // Tags = null
+                Title = "Meu Artigo"
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
-
